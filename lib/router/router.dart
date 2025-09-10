d1b9aeaf63f0f2b9db906fe5ce2f72f593fc5dac
Thisara C Prasanga <thisaraprasanga.dev@gmail.com>
2025-09-10 09:20:29 +0530

    Create custom Error page Using errorPageBuilder

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,9 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:go_routing_app/pages/home_page.dart';
 import 'package:go_routing_app/pages/profile_page.dart';
 
 class RouterClass {
   final router = GoRouter(
+    errorPageBuilder: (context, state) {
+      return MaterialPage<dynamic>(
+        child: Scaffold(
+          body: Center(child: Text("Error page.this page is not found")),
+        ),
+      );
+    },
     initialLocation: "/home",
     routes: [
       GoRoute(path: "/home", builder: (context, state) => HomePage()),
